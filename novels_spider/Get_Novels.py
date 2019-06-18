bc0513b81a995ffd5f11a623aa1a7909f8c67aed
JackyLove <asaltedfishz@gmail.com>
2019-06-18 16:26:16 +0800

    完善爬虫程序

--- novels_spider/Get_Novels.py
+++ novels_spider/Get_Novels.py
@@ -11,8 +11,15 @@ from utils import fn_timer
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36',
-
 }
+proxies = {
+    'http','http://60.162.83.40:9000',
+}
+
+#开始
+def start():
+    print('请输入你需要查询的小说书名关键词或者作者名字：')
+    name = str(input())
 
 
 # 根据书名或者作者名列出笔趣阁网站的所有相关小说
@@ -34,6 +41,9 @@ def parse_html(html):
     if html:
         doc = pq(html.content)
         booklist = doc('.result-game-item-detail')
+        if booklist.text() == '':
+            print('对不起，没有找到您要的书！请输入其它书名')
+            return None
         bookdict = [0]
         resultindex = 1
         for item in booklist.items():
@@ -50,10 +60,12 @@ def parse_html(html):
             print('-' * 20)
             print('\n')
             resultindex += 1
-        print('请输入你想爬取的小说序号(例如1、2...等):')
+        print('请输入你想爬取的小说序号(例如1、2...等)或输入"q"退出程序:')
         while True:
             index = int(input())
-            if index in range(1, len(bookdict)):
+            if str(index)=='q':
+                exit()
+            elif index in range(1, len(bookdict)):
                 break
             else:
                 print('没有您需要的小说序号，请检查输入后重新输入序号！')
@@ -70,22 +82,33 @@ def parse_html(html):
 # axel -n 10
 def getchapter(bookinfo):
     response = requests.get(bookinfo['url'], headers=headers)
-    baseurl = bookinfo['url'] + '/'
+    baseurl = bookinfo['url']
     if response.status_code == 200:
         doc = pq(response.content)
         chapters = doc('dd')
-        bookname = bookinfo['name'] + '.csv'
-        with open(bookname, 'w', encoding='utf-8') as csvfile:
-            writer = csv.writer(csvfile, delimiter=',')
-            writer.writerow(['chapter', 'url'])
-            joined = []
-            for item in chapters.items():
-                url = item('a').attr('href')
-                url = urljoin(baseurl, url)
-                if url not in joined:
-                    writer.writerow([item.text(), url])
-                    joined.append(url)
-        return bookname, len(joined)
+        #不写入文件，直接生成列表
+        joined=[]
+        for item in chapters.items():
+            url = item('a').attr('href')
+            url = urljoin(baseurl,url)
+            # chapter = item.text()
+            if url not in joined:
+                joined.append(url)
+        return joined
+
+
+        # bookname = bookinfo['name'] + '.csv'
+        # with open(bookname, 'w', encoding='utf-8') as csvfile:
+        #     writer = csv.writer(csvfile, delimiter=',')
+        #     writer.writerow(['chapter', 'url'])
+        #     joined = []
+        #     for item in chapters.items():
+        #         url = item('a').attr('href')
+        #         url = urljoin(baseurl, url)
+        #         if url not in joined:
+        #             writer.writerow([item.text(), url])
+        #             joined.append(url)
+        # return bookname, len(joined)
     else:
         print('connection error!')
 
@@ -104,18 +127,19 @@ def getsinglepage(url):
 
 #异步下载测试
 @fn_timer
-def asyncdown(bookcsv,size):
+def asyncdown(urls,filename):
     #将下载链接导入到urls列表中
-    urls = []
-    with open(bookcsv, 'r', encoding='utf-8') as book:
-        reader = csv.reader(book)
-        next(reader)  # 跳过文件头
-        for row in reader:
-            urls.append(row[1])
+    # urls = []
+    # with open(bookcsv, 'r', encoding='utf-8') as book:
+    #     reader = csv.reader(book)
+    #     next(reader)  # 跳过文件头
+    #     for row in reader:
+    #         urls.append(row[1])
     #异步请求
+    size=len(urls)
     ru = (grequests.get(u) for u in urls)
     responses =grequests.map(ru)
-    bookname = bookcsv.split('.')[0] + '.txt'
+    bookname = filename +'.txt'
     with open(bookname, 'a', encoding='utf-8') as writebook:
         completed = 0.0
         count = 1
@@ -170,13 +194,15 @@ def downloadbook(bookcsv, size):
 
 
 if __name__ == '__main__':
-    print('请输入你需要查询的小说书名关键词或者作者名字：')
-    name = str(input())
-    # 获取根据关键词查询的响应
-    results = find_by_name(name)
-    # 解析响应文本，返回用户想下载的小说url
-    bookinfo = parse_html(results)
+    while True:
+        print('请输入你需要查询的小说书名关键词或者作者名字：')
+        name = str(input())
+        # 获取根据关键词查询的响应
+        results = find_by_name(name)
+        # 解析响应文本，返回用户想下载的小说url
+        bookinfo = parse_html(results)
+        if bookinfo is not None:
+            break
     # 根据小说url获取章节和目录信息
-    bookcsv, size = getchapter(bookinfo)
-    # downloadbook(bookcsv, size)
-    asyncdown(bookcsv,size)
+    urls = getchapter(bookinfo)
+    asyncdown(urls,bookinfo['name'])
